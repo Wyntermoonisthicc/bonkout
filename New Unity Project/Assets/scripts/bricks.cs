32997dacacc17cadbf88884a29f3e9d1ddbd1c0c
matthew <mblathwayt@gmail.com>
2019-08-26 11:51:28 +1200

    day 1

---
+++ New Unity Project/Assets/scripts/bricks.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bricks : MonoBehaviour
+{
+    public GameObject brickParticle;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Instantiate(brickParticle, transform.position, Quaternion.identity);
+        gameManager.intstance.DestroyBrick();
+        Destroy(gameObject);
+    }
+
+}
