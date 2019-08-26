5a0b75773adb18bc1bc7bafbe022e07257285c2c
matthew <mblathwayt@gmail.com>
2019-08-27 11:47:55 +1200

    wowee

---
+++ New Unity Project/Assets/scripts/deathzone.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deathzone : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider col)
+    {
+        gameManager.intstance.LoseLife();
+    }
+}
